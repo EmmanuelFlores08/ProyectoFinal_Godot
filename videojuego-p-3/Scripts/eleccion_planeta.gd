893e9eb44d981e8d280f51ca60bff499f88aed2d
EmmanuelFlores08 <emmanuel@estudiohera.mx>
2026-08-19 22:49:25 -0600

    Marcianini Scape

--- videojuego-p-3/Scripts/eleccion_planeta.gd
+++ videojuego-p-3/Scripts/eleccion_planeta.gd
@@ -5,10 +5,12 @@ const PANTALLA_CARGA := "res://Scenes/Pantalla_Cargando.tscn"
 @export_file("*.tscn") var escena_planeta1: String = "res://Scenes/Kalamyr.tscn"
 @export_file("*.tscn") var escena_planeta2: String = "res://Scenes/Fungora.tscn"
 @export_file("*.tscn") var escena_planeta3: String = "res://Scenes/Calcyra.tscn"
+@export_file("*.tscn") var escena_menu: String = "res://Scenes/Menu.tscn"
 
 @onready var b1: BaseButton = $IU/Control/B_Planeta1
 @onready var b2: BaseButton = $IU/Control/B_Planeta2
 @onready var b3: BaseButton = $IU/Control/B_Planeta3
+@onready var boton_menu: BaseButton = $IU/Control/BotonMenu
 
 
 func _ready() -> void:
@@ -16,8 +18,13 @@ func _ready() -> void:
 	b1.pressed.connect(_ir.bind(escena_planeta1))
 	b2.pressed.connect(_ir.bind(escena_planeta2))
 	b3.pressed.connect(_ir.bind(escena_planeta3))
+	boton_menu.pressed.connect(_volver_menu)
 
 
 func _ir(escena: String) -> void:
 	PantallaCargando.siguiente_escena = escena
 	get_tree().change_scene_to_file(PANTALLA_CARGA)
+
+
+func _volver_menu() -> void:
+	get_tree().change_scene_to_file(escena_menu)
--- videojuego-p-3/Scripts/menu.gd
+++ videojuego-p-3/Scripts/menu.gd
@@ -4,12 +4,18 @@ extends Control
 
 @onready var boton_jugar: BaseButton = $Botones/BotonJugar
 @onready var boton_salir: BaseButton = $Botones/BotonSalir
+@onready var boton_creditos: BaseButton = $BotonCreditos
+@onready var canvas_creditos: CanvasLayer = $Creditos
+@onready var boton_cerrar_creditos: BaseButton = $Creditos/BotonCerrar
 
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	canvas_creditos.visible = false
 	boton_jugar.pressed.connect(_jugar)
 	boton_salir.pressed.connect(_salir)
+	boton_creditos.pressed.connect(_abrir_creditos)
+	boton_cerrar_creditos.pressed.connect(_cerrar_creditos)
 
 
 func _jugar() -> void:
@@ -18,3 +24,11 @@ func _jugar() -> void:
 
 func _salir() -> void:
 	get_tree().quit()
+
+
+func _abrir_creditos() -> void:
+	canvas_creditos.visible = true
+
+
+func _cerrar_creditos() -> void:
+	canvas_creditos.visible = false
